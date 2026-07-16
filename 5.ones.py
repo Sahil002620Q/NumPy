6e3425f7daecb3fb2e88d093a6cb6f2e45d2bbc4
Sahil002620Q <smax06388@gmail.com>
2026-07-16 06:18:48 -0500

    same as zeroes but it fill array with 1

---
+++ 5.ones.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print("-------------------------------")
+zero_vector = np.ones(8,dtype=np.int64)     #make zero matrics and tensor
+print(zero_vector,zero_vector.dtype)
+print("-------------------------------")
+zero_matrix = np.ones((2,2),dtype=np.float16)     #add another bracket when giving dimention 
+print(zero_matrix,zero_matrix.dtype)
+print("-------------------------------")
+zero_tensor = np.ones((3,3,3))    
+print(zero_tensor,zero_tensor.dtype)
+print("-------------------------------")
