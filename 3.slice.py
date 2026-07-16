7b1c4faaa38e0c7b7a7fd949935f05682c86491e
Sahil002620Q <smax06388@gmail.com>
2026-07-16 06:15:24 -0500

    slice in numpy , just display the part of array we sliced , did not affect original array , but if we assign array to car then change the value of that new array indirectly original one will be affacted

---
+++ 3.slice.py
@@ -0,0 +1,18 @@
+import numpy as np
+# vector = np.array[0.14,0.23,0.89,0.32,0.02]
+vector = np.array([0,1,2,3,4,5,6,7,8,9])
+
+#all same
+print(vector)
+print(vector[:]) 
+print(vector[::])
+print(vector[0:])
+
+#idk
+print(vector[:0:])
+
+#print form 0 to 5 (last one will not print)
+print(vector[:6])
+
+#cout from 9 to 5 ------------------pending
+print(vector[:])
