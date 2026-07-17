261e7f931724335d40fdb9130f1404e7fbb6d3e3
Sahil002620Q <smax06388@gmail.com>
2026-07-17 21:29:17 +0530

    arange function , used to generate vector of given range

---
+++ 7.arange.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+range = np.arange(10)  # will generate a range from 0 to 9 
+print(range)
+
+ranged = np.arange(5,10)  # will generate a range between 5 to 9
+print(ranged)
+
+stepUp = np.arange(0,11,2)  # will senerate a stepup range ie first element then another consecutive second slement
+print(stepUp)
+
+print(np.__file__) #to see original np file
